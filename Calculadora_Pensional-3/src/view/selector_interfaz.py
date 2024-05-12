385655a2d823270d3da61ccaf77805be829a3123
Matt <116335873+TartarusBoss@users.noreply.github.com>
2024-05-11 19:15:32 -0500

    Add files via upload

---
+++ Calculadora_Pensional-3/src/view/selector_interfaz.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append("C:/Users/ASUS/Calculadora_Pensional-2")
+from view.calculadora_app import CalculadoraPensionalApp
+from view.main import main
+
+def seleccionar_interfaz(opcion):
+    if opcion == "1":
+        main()
+    elif opcion == "2":
+        # Ejecutar la interfaz gráfica
+        app = CalculadoraPensionalApp()
+        app.run()
+    else:
+        print("Opción inválida. Por favor, seleccione 1 o 2.")
+
+if __name__ == "__main__":
+    opcion = input("Seleccione una opción: (1 para interfaz por consola, 2 para interfaz gráfica): ")
+    seleccionar_interfaz(opcion)
